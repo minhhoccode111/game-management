f7bb031c99ca9117cdc5e9cc21b2416d87b6879d
minhhoccode111 <minhhoccode111@gmail.com>
2024-08-15 21:51:05 +0700

    add: restrict and cascade relationship between models

--- Data/GameManagementMvcContext.cs
+++ Data/GameManagementMvcContext.cs
@@ -11,42 +11,50 @@ namespace GameManagementMvc.Data
         // configured explicitly
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // set 2 foreign keys to be primary key
+            // set 2 foreign keys GameId and GenreId to be primary key in GameGenre
             modelBuilder.Entity<GameGenre>().HasKey(gg => new { gg.GenreId, gg.GameId });
 
-            // Game must have at least one Genre
+            // many-to-many: Game-to-Genre (demantory)
             modelBuilder
                 .Entity<GameGenre>()
                 .HasOne(gg => gg.Game)
                 .WithMany(g => g.GameGenres)
                 .HasForeignKey(gg => gg.GameId)
+                // auto remove all GameGenres if delete a Game
+                .OnDelete(DeleteBehavior.Cascade)
                 .IsRequired();
 
-            // Genre exist independent
+            // many-to-many: Genre-to-Game (optional)
             modelBuilder
                 .Entity<GameGenre>()
                 .HasOne(gg => gg.Genre)
                 .WithMany(g => g.GameGenres)
-                .HasForeignKey(gg => gg.GenreId);
+                .HasForeignKey(gg => gg.GenreId)
+                // can't delete without removing all GameGenres first
+                .OnDelete(DeleteBehavior.Restrict);
 
-            // optional: make delete Genre cascade (auto delete all child)
+            // optional: make delete Genre cascade (auto delete all children)
 
-            // Game must have at least one Company
+            // many-to-many: Game-to-Company (demantory)
             modelBuilder
                 .Entity<GameCompany>()
                 .HasOne(gc => gc.Game)
                 .WithMany(g => g.GameCompanies)
                 .HasForeignKey(gc => gc.GameId)
+                // auto remove all GameCompanies if delete a Game
+                .OnDelete(DeleteBehavior.Cascade)
                 .IsRequired();
 
-            // Company exist independent
+            // many-to-many: Company-to-Game (optional)
             modelBuilder
                 .Entity<GameCompany>()
                 .HasOne(gc => gc.Company)
                 .WithMany(c => c.GameCompanies)
-                .HasForeignKey(gc => gc.CompanyId);
+                .HasForeignKey(gc => gc.CompanyId)
+                // can't delete without removing all GameCompanies first
+                .OnDelete(DeleteBehavior.Restrict);
 
-            // optional: make delete Company cascade (auto delete all child)
+            // optional: make delete Company cascade (auto delete all children)
         }
 
         public DbSet<Game> Game { get; set; } = default!;
